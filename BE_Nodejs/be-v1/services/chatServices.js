fb8440a3ed74ce0d5eebb364b9f3fc25ff08097a
hovietvinh <hovietvinh29032004@gmail.com>
2024-08-07 21:49:56 +0700

    be

---
+++ BE_Nodejs/be-v1/services/chatServices.js
@@ -0,0 +1,61 @@
+const Chat = require("../models/chat.model")
+const axios = require("../utils/axios")
+module.exports.createChat =async ()=>{
+    try{
+        const result =await Chat.create({  
+            deleted:false
+        });
+        return result
+        
+
+    }catch(e){
+        console.log(e);
+        return null
+    }
+}
+module.exports.getChatById =async (id)=>{
+    try{
+        
+        let result = await Chat.findOne({_id:id,deleted:false});
+        return result;
+    }catch(e){
+        console.log("error find chat");
+        return null
+    }
+}
+
+module.exports.addMessage =async (chatUser,id)=>{
+    try{
+        const response = await axios.post('http://localhost:5000/api/chat', { message: chatUser });
+        let result = await Chat.updateOne({_id:id},{
+            $push:{
+                chatUser:chatUser,
+                chatMachine: response.response
+            }
+        })
+        return response.response;
+    }catch(e){
+        console.log("error add chat");
+        return null
+    }
+}
+
+module.exports.deleteMessage =async (id)=>{
+    try{
+        let result = await Chat.updateOne({_id:id},{deleted:true})
+        return result;
+    }catch(e){
+        console.log("error del chat");
+        return null
+    }
+}
+
+module.exports.getAllChat =async ()=>{
+    try{
+        let result = await Chat.find({deleted:false});
+        return result;
+    }catch(e){
+        console.log("error find all chat");
+        return null
+    }
+}
